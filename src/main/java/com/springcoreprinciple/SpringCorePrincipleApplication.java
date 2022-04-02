4d10dd7081968b97fac6126366749ad513ea2f87
Yoonseok Jo <forstardom@gmail.com>
2022-04-02 23:12:53 +0900

    2022-04-02

---
+++ src/main/java/com/springcoreprinciple/SpringCorePrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.springcoreprinciple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCorePrincipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCorePrincipleApplication.class, args);
+	}
+
+}
