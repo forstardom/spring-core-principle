16f7858343dad940620a79aadb2fd2a3c09d37f9
Yoonseok Jo <forstardom@gmail.com>
2022-04-05 23:12:43 +0900

    2022-04-05

--- src/main/java/com/springcoreprinciple/AppConfig.java
+++ src/main/java/com/springcoreprinciple/AppConfig.java
@@ -1,20 +1,37 @@
 package com.springcoreprinciple;
 
+import com.springcoreprinciple.discount.DiscountPolicy;
 import com.springcoreprinciple.discount.FixDiscountPolicy;
+import com.springcoreprinciple.discount.RateDiscountPolicy;
 import com.springcoreprinciple.member.MemberService;
 import com.springcoreprinciple.member.MemberServiceImpl;
 import com.springcoreprinciple.member.MemoryMemberRepository;
 import com.springcoreprinciple.order.OrderService;
 import com.springcoreprinciple.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
--- src/main/java/com/springcoreprinciple/MemberApp.java
+++ src/main/java/com/springcoreprinciple/MemberApp.java
@@ -4,13 +4,21 @@ import com.springcoreprinciple.member.Grade;
 import com.springcoreprinciple.member.Member;
 import com.springcoreprinciple.member.MemberService;
 import com.springcoreprinciple.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
